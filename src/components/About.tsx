173c5053a0465786029b3b73a132e53be3af1c59
mauromltn <mauromtne@icloud.com>
2025-04-07 18:23:18 +0200

    feat: Add responsiveness to about component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,32 +3,32 @@ import Divider from "./ui/Divider"
 export default function About() {
    return (
       <section id="about">
-         <div className="w-150 font-light">
+         <div className="md:w-160 font-light">
             <h1>ABOUT US</h1>
-            <p className="mt-4 text-4xl">
+            <p className="mt-3 text-2xl md:text-4xl">
                We are a passionate team of travel enthusiasts dedicated to making your travel dreams come true.
-               <span className="text-[var(--secondary)]">Our mission is to provide you with the best travel experiences.</span>
+               <span className="text-[var(--secondary)]"> Our mission is to provide you with the best travel experiences.</span>
             </p>
          </div>
 
-         <div className="flex items-center justify-around text-center md:w-150 h-15">
+         <div className="flex items-center justify-center text-center w-full h-10 gap-3 text-2xl md:w-140 md:h-15 md:gap-6 md:text-4xl">
             <div>
-               <span className="text-4xl">200+</span>
-               <p className="text-[var(--tertiary)]">Happy Customers</p>
+               <span>200+</span>
+               <p className="text-[var(--tertiary)] text-sm md:text-base">Happy Customers</p>
             </div>
 
             <Divider className="bg-[var(--secondary)] h-1/2" />
             
             <div>
-               <span className="text-4xl">65+</span>
-               <p className="text-[var(--tertiary)]">Top Hotels</p>
+               <span>65+</span>
+               <p className="text-[var(--tertiary)] text-sm md:text-base">Top Hotels</p>
             </div>
 
             <Divider className="bg-[var(--secondary)] h-1/2" />
             
             <div>
-               <span className="text-4xl">250+</span>
-               <p className="text-[var(--tertiary)]">Experienced Guide</p>
+               <span>250+</span>
+               <p className="text-[var(--tertiary)] text-sm md:text-base">Experienced Guide</p>
             </div>
          </div>
       </section>

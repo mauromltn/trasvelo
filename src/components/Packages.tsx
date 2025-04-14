963275a1b8880eba631b75a13afc9bd976bcb2fd
mauromltn <mauromtne@icloud.com>
2025-04-14 23:44:49 +0200

    feat: Add travel packages feature
    - Create Packages component
    - Animate the cards on hover
    - Create data file for cards info

--- src/components/Packages.tsx
+++ src/components/Packages.tsx
@@ -1,13 +1,35 @@
+import { packagesInfo } from '@/data/packagesInfo';
+import Image from 'next/image';
+
 export default function Packages() {
    return (
       <section
          id="packages"
-         className="flex-col"
+         className="flex-col items-center"
       >
-         <h1>OUR PACKAGES</h1>
-         <h2 className="text-4xl lg:text-[3.2rem]">Discover our exceptional selection of travel packages and destinations.</h2>
+         <div className="text-center max-w-300 mb-10">
+            <h1>OUR PACKAGES</h1>
+            <h2 className="text-4xl lg:text-[3.2rem]">Discover our exceptional selection of travel packages and destinations.</h2>
+         </div>
 
-
+         <div className="cardContainer text-white">
+            {packagesInfo.map((packageItem, id) => (
+               <div key={id} className="packageCard relative">
+                  <span className="text-sm">{packageItem.time}</span>
+                  <Image
+                     src={packageItem.src}
+                     alt={packageItem.alt}
+                     fill
+                     objectFit="cover"
+                     className="brightness-70 rounded-2xl"
+                  />
+                    <div>
+                      <h3 className="text-xl font-bold">{packageItem.header}</h3>
+                      <p>{packageItem.description}</p>
+                    </div>
+               </div>
+            ))}
+         </div>
       </section>
    )
 }
---
+++ src/data/packagesInfo.ts
@@ -0,0 +1,23 @@
+export const packagesInfo = [
+   {
+      src: "/packages/allInclusive.jpg",
+      alt: "A beautiful mountain view",
+      header: "Mountain Adventure",
+      description: "Explore the breathtaking mountain landscapes with our guided tours.",
+      time: "3 days"
+   },
+   {
+      src: "/packages/adventure.jpg",
+      alt: "A serene beach",
+      header: "Beach Getaway",
+      description: "Relax on pristine beaches and enjoy the calming ocean breeze.",
+      time: "5 days"
+   },
+   {
+      src: "/packages/culture.jpg",
+      alt: "A bustling cityscape",
+      header: "City Escape",
+      description: "Discover the vibrant life and culture of the world's greatest cities.",
+      time: "2 days"
+   }
+]

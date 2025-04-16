9408fcd21a507dd3bd9e69257d56807ee26f4d30
mauromltn <mauromtne@icloud.com>
2025-04-16 14:28:22 +0200

    fix: Adjust text sizes and button styles in Packages component for better responsiveness

--- src/components/Packages.tsx
+++ src/components/Packages.tsx
@@ -15,7 +15,7 @@ export default function Packages() {
          <div className="flex flex-col lg:flex-row cardContainer text-white">
             {packagesInfo.map((packageItem, id) => (
                <div key={id} className="packageCard relative">
-                  <span className="blurDiv text-sm py-2 px-2.5">5 days, 4 nights</span>
+                  <span className="blurDiv text-xs md:text-sm py-2 px-2.5">5 days, 4 nights</span>
                   <Image
                      src={packageItem.src}
                      alt={packageItem.alt}
@@ -25,12 +25,12 @@ export default function Packages() {
                   />
                   <div className='flex justify-between items-center'>
                       <div>
-                        <h3 className="text-2xl">{packageItem.header}</h3>
-                        <p className='max-w-80 font-light'>{packageItem.description}</p>
+                        <h3 className="text-[1.3rem] font-medium md:text-2xl mb-1 md:mb-0">{packageItem.header}</h3>
+                        <p className='max-w-80 text-sm md:text-base font-light'>{packageItem.description}</p>
                       </div>
 
-                      <div className='packageButton absolute bottom-30 md:bottom-35 lg:bottom-30 xl:bottom-10 xl:right-10 scale-100 lg:scale-0 duration-300 ease'>
-                        <button className='blurDiv py-2.5 px-6 font-light cursor-pointer'>Choose package</button>
+                      <div className='packageButton absolute bottom-28 md:bottom-35 lg:bottom-30 xl:bottom-10 xl:right-10 scale-100 lg:scale-0 duration-300 ease'>
+                        <button className='blurDiv py-2.5 px-4 md:px-6 text-sm md:text-vase font-light cursor-pointer'>Choose package</button>
                       </div>
                   </div>
                </div>

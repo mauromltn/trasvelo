998d65094046411572be4f6b0892ce8161e93d0d
Mauro <116788542+mauromltn@users.noreply.github.com>
2025-04-20 14:25:14 +0000

    feat: Add responsiveness to CallToAction component

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -4,7 +4,7 @@ import Divider from "./ui/Divider"
 
 export default function CallToAction() {
    return (
-      <section className="relative flex justify-center w-full lg:h-180 text-white mt-20">
+      <section className="relative flex justify-center w-full h-120 md:h-180 text-white mt-20">
          <Image
             src={callToAction}
             placeholder='blur'
@@ -13,21 +13,29 @@ export default function CallToAction() {
             className=" brightness-75 rounded-[25] border-5 md:rounded-[35] md:border-15"
             alt="Call to action image"
          />
-         <span className="absolute top-50 text-center w-310 text-[4rem] tracking-tight">
+         <span className="absolute top-33 sm:top-40 md:top-60 lg:top-50 text-center w-90 sm:w-130 md:w-190 lg:w-250 xl:w-310 text-[1.5rem] md:text-[2.3rem] lg:text-[3rem] xl:text-[4rem] font-medium md:font-normal tracking-tight">
             Don&apos;t wait any longer! Start your adventure and explore new experiences today
          </span>
 
-         <div className="absolute top-110 flex items-center w-120 h-15 p-2 gap-8 rounded-full border-2 border-white text-sm font-medium backdrop-blur-sm bg-black/5">
+         <div className="absolute top-68 md:top-100 xl:top-110 flex items-center w-75 h-12 md:w-120 md:h-15 p-1.5 md:p-2 gap-3 md:gap-8 rounded-full border-2 border-white text-sm font-semibold backdrop-blur-sm bg-black/5">
             <input
                type="email"
                name="email"
                placeholder="Drop your email address here..."
-               className="w-2/3 py-2 pl-4 text-sm font-semibold placeholder-light rounded-full focus:outline-none"
+               className="hidden md:block w-2/3 pl-4 rounded-full focus:outline-none"
             />
 
-            <Divider className="h-8 bg-white mr-4" />
+            { /* input per desktop */ }
+            <input
+               type="email"
+               name="email"
+               placeholder="Drop your email here..."
+               className="md:hidden w-2/3 pl-4 text-xs rounded-full focus:outline-none"
+            />
 
-            <button className="w-1/3 h-full py-2 px-4 bg-white text-black text-sm font-semibold rounded-full hover:bg-gray-300 duration-200 ease cursor-pointer">
+            <Divider className="h-5 md:h-8 bg-white mr-2 md:mr-4" />
+
+            <button className="w-1/3 h-full md:py-2 px-2 bg-white text-xs text-black rounded-full hover:bg-gray-300 duration-200 ease cursor-pointer">
                Subscribe
             </button>
          </div>
